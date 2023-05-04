b25f0b3530919d932de5df75f5d52e62541fa754
Mohamed Arif <a.mdarif10@yahoo.com>
2023-05-05 01:13:43 +0530

    Adding the ability to search for cities.

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 API_KEY = '7d2ccf162636befe1223b1483251a6ab'
-city = 'London'
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
 
-
-@app.route('/')
-def index():
-    response = requests.get(url)
-    data = response.json()
-    temperature = data['main']['temp']
-    print(f"The current temperature in {city} is {temperature}°C")
-    return render_template('index.html', temperature=temperature)
+@app.route("/", methods=["GET", "POST"])
+def get_weather():
+    if request.method == "POST":
+        city = request.form["city"]
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        weather_data = response.json()
+        temperature = weather_data["main"]["temp"]
+        return render_template("index.html", temperature=temperature, city=city)
+    else:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        weather_data = response.json()
+        temperature = weather_data["main"]["temp"]
+        return render_template("index.html", temperature=temperature, city='London')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
