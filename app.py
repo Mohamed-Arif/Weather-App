80562bda07530cc88b7e8ebcd34bcc8650235477
Mohamed Arif <a.mdarif10@yahoo.com>
2023-05-27 23:02:07 +0530

    Added 'feels like temp'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -15,7 +15,11 @@ def index():
             response = requests.get(weather_url)
             data = response.json()
             current_temperature = data['main']['temp']
-            return str(current_temperature)
+            feels_like_temperature = data['main']['feels_like']
+            return jsonify({
+                'currentTemperature': current_temperature,
+                'feelsLikeTemperature': feels_like_temperature
+            })
         except KeyError:
             return 'Invalid city name or weather data not available'
         except requests.exceptions.RequestException as e:
